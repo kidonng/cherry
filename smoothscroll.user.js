bd15548447cb2fbcebcd9562f64167718a4f34f8
Kid <kidonng@gmail.com>
2019-04-08 21:41:35 +0800

    Add Smooth Scroll

---
+++ smoothscroll.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         Smooth Scroll
+// @version      1.0.0
+// @description  Alternative to the Smooth Scroll Chrome extension
+// @author       kidonng
+// @include      *
+// @require      https://cdn.jsdelivr.net/npm/smoothscroll-for-websites/SmoothScroll.min.js
+// ==/UserScript==
