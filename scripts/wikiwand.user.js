d1386451e23313d51c6b8e527341f0bff0a9a730
Kid <kidonng@gmail.com>
2019-06-08 21:33:33 +0800

    Wikiwand: init

---
+++ scripts/wikiwand.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Wikiwand
+// @version      1.0.0
+// @description  Replace Wikiwand browser extension
+// @author       kidonng
+// @include      https://*.wikipedia.org/*
+// @include      https://www.wikiwand.com/*
+// ==/UserScript==
+
+if (location.host.includes('wikipedia.org') && !location.href.includes('oldformat=true')) {
+  const path = location.pathname.substring(1, location.pathname.indexOf('/', 1))
+  const lang = new URLSearchParams(location.search).get('variant') || (path === 'wiki' ? location.host.substring(0, location.host.indexOf('.')) : path)
+  const hash = location.hash.replace('cite_note-', 'citenote')
+  const title = location.pathname.includes('index.php') ? new URLSearchParams(location.search).get('title') : location.pathname.substring(location.pathname.indexOf('/', 1) + 1)
+  location.href = `https://www.wikiwand.com/${lang}/${title}${hash}`
+} else if (location.host === 'www.wikiwand.com' && location.hash) {
+  setTimeout(() => document.querySelector(location.hash.replace('/', '')).scrollIntoView(), 0)
+}
