feb8519488fbb9e6fade0ed31192c32cf7ef432a
Kid <kidonng@gmail.com>
2019-06-30 18:02:04 +0800

    Tieba Redirect: rewrite using `switch`
    close #5

--- scripts/tieba-redirect.user.js
+++ scripts/tieba-redirect.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Tieba Redirect
-// @version      1.1.0
+// @version      1.1.1
 // @description  Redirect all variants of Tieba site to tieba.baidu.com
 // @author       kidonng
 // @include      http*://tieba.baidu.com/*
@@ -12,27 +12,30 @@
 // ==/UserScript==
 
 ;(() => {
-  if (location.host === 'tieba.baidu.com') {
-    if (location.pathname.startsWith('/mo')) {
-      const params = new URLSearchParams(location.search.substring(1))
-      const prefix = 'https://tieba.baidu.com/'
-      if (params.get('word'))
-        location.href = `${prefix}f?kw=${params.get('word')}`
-      else if (params.get('kz'))
-        location.href = `${prefix}p/${params.get('kz')}?pn=${Math.ceil(
-          params.get('pn') / 30
-        ) + 1}`
-    } else PageData.user.is_login = true
-  } else if (
-    location.host === 'dq.tieba.com' ||
-    location.host === 'jump2.bdimg.com' ||
-    location.host === 'c.tieba.baidu.com' ||
-    location.host === 'wefan.baidu.com'
-  )
-    location.host = 'tieba.baidu.com'
-  else if (location.href.includes('wapp.baidu.com/mo/q/m'))
-    location.href = location.href.replace(
-      'wapp.baidu.com/mo/q/m',
-      'tieba.baidu.com/f'
-    )
+  switch (location.host) {
+    case 'tieba.baidu.com':
+      if (location.pathname.startsWith('/mo')) {
+        const params = new URLSearchParams(location.search.substring(1))
+        const prefix = 'https://tieba.baidu.com/'
+        if (params.get('word'))
+          location.href = `${prefix}f?kw=${params.get('word')}`
+        else if (params.get('kz'))
+          location.href = `${prefix}p/${params.get('kz')}?pn=${Math.ceil(
+            params.get('pn') / 30
+          ) + 1}`
+      } else PageData.user.is_login = true
+      break
+    case 'dq.tieba.com':
+    case 'jump2.bdimg.com':
+    case 'c.tieba.baidu.com':
+    case 'wefan.baidu.com':
+      location.host = 'tieba.baidu.com'
+      break
+    case (location.href.includes('wapp.baidu.com/mo/q/m')):
+      location.href = location.href.replace(
+        'wapp.baidu.com/mo/q/m',
+        'tieba.baidu.com/f'
+      )
+      break
+  }
 })()
