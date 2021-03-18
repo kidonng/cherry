892135669c92e90595fcb69ea2883db7f5e7d523
Kid <44045911+kidonng@users.noreply.github.com>
2021-03-18 18:16:00 +0800

    GitHub profile README link: fix selector

--- scripts/github-profile-readme-link.user.js
+++ scripts/github-profile-readme-link.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GitHub profile README link
-// @version      1.0.0
+// @version      1.0.1
 // @description  Make profile README's header link to the README instead of the repository
 // @author       kidonng
 // @namespace    https://github.com/kidonng/cherry
@@ -10,7 +10,7 @@
 ;(() => {
   'use strict'
 
-   const link = document.querySelector('.user-profile-nav + div .octicon-smiley + a')
+   const link = document.querySelector('.user-profile-nav + div .octicon-octoface + a')
 
    if (link) {
      link.href += '#readme'
