ca27ef6940cde8a4cef3f2ebd6d7c9c096f59f9a
Kid <kidonng@gmail.com>
2021-08-03 10:56:30 +0800

    Clash: get rules from default config.yaml

--- deno/utils/clash.ts
+++ deno/utils/clash.ts
@@ -3,13 +3,10 @@ import { existsSync } from 'https://deno.land/std@0.91.0/fs/exists.ts'
 import yaml from 'https://cdn.skypack.dev/js-yaml'
 
 export function generateConfig(proxies: Record<string, unknown>): string {
-  const config = join(Deno.env.get('HOME')!, '.config', 'clash', 'private.yaml')
+  const config = join(Deno.env.get('HOME')!, '.config', 'clash', 'config.yaml')
   const rules = existsSync(config)
     ? yaml.load(Deno.readTextFileSync(config)).rules
-    : [
-      'GEOIP,CN,DIRECT',
-      'MATCH,PROXY'
-    ]
+    : ['GEOIP,CN,DIRECT', 'MATCH,PROXY']
 
   return yaml.dump({ ...proxies, rules })
 }
