46cb1575f731a6a363ec5c360d77338fc974eb06
Kid <kidonng@gmail.com>
2021-08-06 21:10:24 +0800

    HTTPS Everywhere: memorize failed redirects

--- scripts/https-everywhere.user.js
+++ scripts/https-everywhere.user.js
@@ -1,19 +1,35 @@
 // ==UserScript==
 // @name         HTTPS Everywhere
-// @version      1.1.3
+// @version      1.2.0
 // @description  Redirect to HTTPS version if available
 // @author       kidonng
 // @namespace    https://github.com/kidonng/cherry
 // @match        http://*/*
+// @match        https://*/*
 // @exclude      http://localhost*
 // @run-at       document-start
+// @grant        GM_getValue
+// @grant        GM_setValue
 // @noframes
 // ==/UserScript==
 
 ;(() => {
+  const domains = GM_getValue('domains', {})
+
+  if (domains[location.hostname] === false) {
+    if (location.protocol === 'https:') {
+      domains[location.hostname] = true
+      GM_setValue('domains', domains)
+    }
+
+    return
+  }
+
   try {
     const https = location.href.replace('http', 'https')
-    fetch(https, { mode: 'no-cors' })
-      .then(() => location.replace(https))
+    fetch(https, { mode: 'no-cors' }).then(() => {
+      GM_setValue('domains', { ...domains, [location.hostname]: false })
+      location.replace(https)
+    })
   } catch (e) {}
 })()
