cdccd1a1fdbf3d3f6f44c1e438b5da0152a8f89f
Kid <kidonng@gmail.com>
2021-11-06 23:56:12 +0800

    GitHub Hovercards: use `dataset`

--- scripts/github-hovercards.user.ts
+++ scripts/github-hovercards.user.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GitHub Hovercards
-// @version      2
+// @version      3
 // @description  Enable native hovercards for more GitHub links
 // @author       kidonng
 // @namespace    https://github.com/kidonng/cherry
@@ -11,32 +11,34 @@
 import { observe } from 'selector-observer'
 import * as detect from 'github-url-detection'
 
-// Other types of hovercards are enabled on most pages so don't bother to enable them
-document
-    .querySelector('.application-main')!
-    .setAttribute('data-repository-hovercards-enabled', '')
-
 observe(
     `a:is([href^="/"], [href^="${location.origin}"]):not(:is([data-hovercard-type], [data-pjax]))`,
     {
         constructor: HTMLAnchorElement,
         add(link) {
             let { pathname } = link
+            let type = ''
 
-            if (detect.isRepoRoot(link))
-                link.setAttribute('data-hovercard-type', 'repository')
+            if (detect.isRepoRoot(link)) type = 'repository'
             else if (detect.isCommit(link)) {
-                link.setAttribute('data-hovercard-type', 'commit')
+                type = 'commit'
 
                 if (detect.isPRCommit(link))
                     pathname = pathname.replace(/pull\/\d+\/commits/, 'commit')
-            } else if (detect.isIssue(link))
-                link.setAttribute('data-hovercard-type', 'issue')
-            else if (detect.isPRConversation(link))
-                link.setAttribute('data-hovercard-type', 'pull_request')
+            } else if (detect.isIssue(link)) type = 'issue'
+            else if (detect.isPRConversation(link)) type = 'pull_request'
 
-            if (link.hasAttribute('data-hovercard-type'))
-                link.setAttribute('data-hovercard-url', `${pathname}/hovercard`)
+            if (type) {
+                link.dataset[
+                    `${
+                        type === 'issue' || type === 'pull_request'
+                            ? 'issueAndPr'
+                            : type
+                    }HovercardsEnabled`
+                ] = ''
+                link.dataset['hovercardType'] = type
+                link.dataset['hovercardUrl'] = `${pathname}/hovercard`
+            }
         },
     }
 )
