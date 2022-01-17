7c0d32edc295629524635c5d4ac8188ca803208e
Kid <kidonng@gmail.com>
2022-01-17 20:31:01 +0800

    GitHub conversation list avatars: work around `highlight-collaborators-and-own-conversations` style
    If anything, this is the *real* fix

--- scripts/github-conversation-list-avatars.user.tsx
+++ scripts/github-conversation-list-avatars.user.tsx
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GitHub conversation list avatars
-// @version      3
+// @version      4
 // @description  Add avatars in GitHub's conversation list
 // @author       kidonng
 // @namespace    https://github.com/kidonng/cherry
@@ -11,6 +11,9 @@ import React from 'dom-chef'
 import { observe } from 'selector-observer'
 
 const className = 'gcla-processed'
+
+// https://github.com/refined-github/refined-github/blob/82fb3d62f11838ad4120f510bd90520c57bb12da/source/features/highlight-collaborators-and-own-conversations.css#L4
+document.head.append(<style>{`.${className}.rgh-collaborator { padding: 2px 5px 4px; }`}</style>)
 
 observe(
     `:is(.js-issue-row, .js-pinned-issue-list-item) [data-hovercard-type="user"]:not(.${className})`,
