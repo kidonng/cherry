3f72ddaf8bedba124e3fc7ed7789700e7eec525f
Kid <hi@xuann.wang>
2022-03-11 01:22:44 +0800

    Improve build scripts

--- scripts/config/watch.ts
+++ scripts/config/watch.ts
@@ -1,21 +1,22 @@
-import { colors } from './deps.ts'
-import { build, config } from './esbuild.ts'
+import { colors, esbuild } from './deps.ts'
+import { config } from './esbuild.ts'
 
 console.log('Started watching')
 
-for (const [scripts, options] of config) {
-    for (const script of scripts) {
-        build(script, {
-            watch: {
-                onRebuild() {
-                    console.log(
-                        `[${new Date().toLocaleTimeString()}] Building ${colors.bold(
-                            script
-                        )}`
-                    )
-                },
+for (const options of config) {
+    const [script] = options.entryPoints as string[]
+    esbuild.build({
+        ...options,
+        watch: {
+            onRebuild() {
+                console.log(
+                    `[${new Date().toLocaleTimeString()}] Building ${colors.bold(
+                        script
+                    )}`
+                )
             },
-            ...options,
-        })
-    }
+        },
+    })
 }
+
+// No `esbuild.stop()` here since we have to Ctrl-C anyway
