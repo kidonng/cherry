758e110d5e7b87029c55c0f15947019290da8b9d
Kid <hi@xuann.wang>
2022-06-04 12:28:31 +0800

    Block Notion Analytics: remove unnecessary closure

--- scripts/block-notion-analytics.user.ts
+++ scripts/block-notion-analytics.user.ts
@@ -9,47 +9,45 @@
 // @run-at       document-start
 // ==/UserScript==
 
-;(() => {
-    const json = (data: unknown) => new Response(JSON.stringify(data))
-    // `window.CONFIG.api.http`
-    const api = '/api/v3'
+const json = (data: unknown) => new Response(JSON.stringify(data))
+// `window.CONFIG.api.http`
+const api = '/api/v3'
 
-    const rawFetch = window.fetch
-    // deno-lint-ignore require-await
-    window.fetch = async (...args) => {
-        const url = args[0] as string
+const rawFetch = window.fetch
+// deno-lint-ignore require-await
+window.fetch = async (...args) => {
+    const url = args[0] as string
 
-        if (url === `${api}/getUserAnalyticsSettings`) {
-            return json({
-                isAmplitudeEnabled: false,
-                isIntercomEnabled: false,
-                isSegmentEnabled: false,
-            })
-        } else if (
-            [
-                `${api}/identifySegmentWorkspace`,
-                `${api}/trackSegmentEvent`,
-            ].includes(url)
-        ) {
-            return json({})
-        } else if (url === 'https://api.statsig.com/v1/rgstr') {
-            return json({ success: true })
-        }
-
-        return rawFetch(...args)
+    if (url === `${api}/getUserAnalyticsSettings`) {
+        return json({
+            isAmplitudeEnabled: false,
+            isIntercomEnabled: false,
+            isSegmentEnabled: false,
+        })
+    } else if (
+        [
+            `${api}/identifySegmentWorkspace`,
+            `${api}/trackSegmentEvent`,
+        ].includes(url)
+    ) {
+        return json({})
+    } else if (url === 'https://api.statsig.com/v1/rgstr') {
+        return json({ success: true })
     }
 
-    Object.defineProperty(window, '__SENTRY__', {
-        get() {
-            return {
-                hub: {
-                    isOlderThan() {},
-                    getScope() {},
-                    bindClient() {},
-                    configureScope() {},
-                },
-            }
-        },
-        set() {},
-    })
-})()
+    return rawFetch(...args)
+}
+
+Object.defineProperty(window, '__SENTRY__', {
+    get() {
+        return {
+            hub: {
+                isOlderThan() {},
+                getScope() {},
+                bindClient() {},
+                configureScope() {},
+            },
+        }
+    },
+    set() {},
+})
