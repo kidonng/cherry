6c8e078d3b7fd3fa3b64f2d2af167c8309dd9d28
Kid <hi@xuann.wang>
2023-02-14 19:50:26 +0800

    Restore Notion Updates tabs: fix page updates

--- scripts/restore-notion-updates-tabs.user.ts
+++ scripts/restore-notion-updates-tabs.user.ts
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Restore Notion Updates tabs
-// @version      2
+// @version      3
 // @description  Restore “All” and “Following” updates tab in Notion
 // @author       kidonng
 // @namespace    https://github.com/kidonng/cherry
@@ -8,16 +8,26 @@
 // @run-at       document-start
 // ==/UserScript==
 
-const json = (data = {}) => new Response(JSON.stringify(data))
 const rawFetch = globalThis.fetch
+// Differentiate from page updates
+const isUpdatesOpen = () => document.querySelector('.notion-updates-menu')
 
 globalThis.fetch = async (url, options) => {
 	if (url === '/api/v3/getActivityLog') {
-		const body = JSON.parse(options!.body as string) as {
-			navigableBlock: unknown
+		if (!isUpdatesOpen())
+			// Wait for first render
+			// This poses a small time tax on page updates
+			await new Promise((resolve) => {
+				setTimeout(resolve)
+			})
+
+		if (isUpdatesOpen()) {
+			const body = JSON.parse(options!.body as string) as {
+				navigableBlock: unknown
+			}
+			delete body.navigableBlock
+			options!.body = JSON.stringify(body)
 		}
-		delete body.navigableBlock
-		options!.body = JSON.stringify(body)
 	}
 
 	if (url === 'https://exp.notion.so/v1/initialize') {
@@ -26,7 +36,7 @@ globalThis.fetch = async (url, options) => {
 		// Hashed "notifs_remove_following_tab"
 		data.feature_gates['kV1Z+fxPuB3RU8MBmr/wnXKofEMdxXmXkWw07RJe6L4='].value =
 			false
-		return json(data)
+		return new Response(JSON.stringify(data))
 	}
 
 	return rawFetch(url, options)
